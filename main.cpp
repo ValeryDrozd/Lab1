2db5f3a15b63b4879dd80b19054f1daf0d83b1c5
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-03-04 01:07:25 +0200

    Counting votes of countries

--- main.cpp
+++ main.cpp
@@ -13,7 +13,14 @@ void readCsvFile(char filepath[],char filename[])
     string fin = s1+"\\"+s2;
 }
 
-
+bool cmp(country a,country b){
+    int sa = 0,sb = 0;
+    for(int i=0;i<20;i++){
+        sa+=a.votes[i];
+        sb+=b.votes[i];
+    }
+    return sa>sb;
+}
 int main()
 {
 
